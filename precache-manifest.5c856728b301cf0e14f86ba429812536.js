2197d33a45963cc2811ed533da0460b20c3ef939
Xavier <xavier@skillup.co>
2019-01-15 10:36:36 +0100

    Updates

---
+++ precache-manifest.5c856728b301cf0e14f86ba429812536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4841b1afcee72cbc4369",
+    "url": "/Dailypedia/static/js/runtime~main.4841b1af.js"
+  },
+  {
+    "revision": "557605470c84e1a5bc3d",
+    "url": "/Dailypedia/static/js/main.55760547.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/Dailypedia/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "557605470c84e1a5bc3d",
+    "url": "/Dailypedia/static/css/main.a426ba25.chunk.css"
+  },
+  {
+    "revision": "b5ed5ece302dd19f5b0d1d86685d1e4d",
+    "url": "/Dailypedia/index.html"
+  }
+];
