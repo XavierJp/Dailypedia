53be385b389f223e8e0715cc075a61f9db70bfda
Xavier <xavier@skillup.co>
2019-01-18 15:07:40 +0100

    fixed random buttom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const zap = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    class="feather feather-zap"
+  >
+    <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+  </svg>
+);
 class App extends Component {
   constructor(props) {
     super(props);
@@ -94,7 +110,7 @@ class App extends Component {
         </div>
         <div className="random">
           <button type="submit" onClick={this.getRandomUrl}>
-            Random
+            {zap} Article aléatoire
           </button>
         </div>
         {body && body.parse && (
