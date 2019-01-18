28f8a7a09bafbfd2facbc9c3cb09ca0f59f94b6d
Xavier <xavier@skillup.co>
2019-01-18 12:54:12 +0100

    scroll bar

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ class App extends Component {
     super(props);
 
     this.state = {
-      loading: true
+      loading: true,
+      pathName: window.location.pathname.replace('/wiki/', '') || ''
     };
     this.fetchWikiData();
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      if (!this.progressBar) {
+        return;
+      }
+      const winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrolled = (winScroll / height) * 100;
+      this.progressBar.style.width = Math.round(scrolled) + '%';
+    });
   }
 
   fetchWikiData = (subject = 'Fonction_à_sens_unique') => {
     const myHeaders = new Headers();
     myHeaders.append('Origin', '*');
 
-    const sujet = (this.subject && this.subject.value) || subject;
+    const sujet =
+      this.state.pathName || (this.subject && this.subject.value) || subject;
     fetch(
       // 'https://fr.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5&format=json&origin=*',
       `https://fr.wikipedia.org/w/api.php?action=parse&page=${sujet}&format=json&origin=*`,
@@ -40,8 +57,17 @@ class App extends Component {
       <div className="App">
         {body && (
           <>
-            <input ref={el => (this.subject = el)} />
-            <button onClick={this.fetchWikiData}>Go</button>
+            <div className="progress-bar">
+              <div ref={el => (this.progressBar = el)} />
+            </div>
+            <input
+              type="text"
+              ref={el => (this.subject = el)}
+              placeholder="Rechercher un article"
+            />
+            <button type="submit" onClick={this.fetchWikiData}>
+              Go
+            </button>
             <h1>{body.parse.title}</h1>
             <div
               dangerouslySetInnerHTML={{
